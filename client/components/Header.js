3fecce282e8dd6e56fd0a12f4af7a1c34f970982
maxmomo <32480503+maxmomo@users.noreply.github.com>
2025-01-05 15:57:01 +0100

    [ADD] Ajout des crédits dans le Header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, Dimensions, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useMyContext } from '../context/MyContext';
 
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import colors from '../constants/colors';
 
 export default function Header({ showBackButton = true, onProfilePress }) {
     const navigation = useNavigation();
+    const { state } = useMyContext();
     const { width, height } = Dimensions.get('window'); 
+
+    const credit = state['user']['credit']
 
     return (
         <View style={[styles.container, {height: height * 0.13}]}>
@@ -19,6 +23,12 @@ export default function Header({ showBackButton = true, onProfilePress }) {
             ) : (
                 <View style={styles.leftButtonSection} />
             )}
+
+            {/* Crédits du joueur */}
+            <View style={styles.creditSection}>
+                <MaterialCommunityIcons name='cash' size={height * 0.04} color={colors.theme} />
+                <Text style={styles.credtiText}>{credit.toLocaleString()}</Text>
+            </View>
 
             {/* Bouton Profil */}
             <TouchableOpacity style={styles.rightButtonSection}>
@@ -47,8 +57,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    buttonText: {
-        fontSize: 16,
-        color: colors.whiteText,
+    creditSection: {
+        marginTop: '5%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row'
     },
+    credtiText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: colors.theme
+    }
 });
